43b593d4a6d6a6b7bfc723daa46a678fb84261a9
TecHDuDe21 <varunv2506@gmail.com>
2026-03-13 14:58:40 +0530

    UC2

--- HelloApp.java
+++ HelloApp.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 public class HelloApp{
  public static void main(String[] args){
-     System.out.println("Hello World");
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the name");
+    String userName = sc.nextline();
+     System.out.println(userName);
     }
 }
